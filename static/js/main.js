86b3d354e460f6f1b05df9da3a6aaf898bc5fd63
Kendzioor <wojtekadam1@gmail.com>
2019-07-24 16:33:19 +0200

    Add new function for add NewCard Button

--- static/js/main.js
+++ static/js/main.js
@@ -7,19 +7,21 @@ function init() {
     // loads the boards to the screen
     // dom.loadBoards();
     addNewBoard();
+    addNewCard();
 }
 
 init();
 
 function addNewBoard() {
-    let newBoardButton = document.getElementById('top_button');
+    let newBoardButton = document.getElementById('addNewBoardBtn');
     newBoardButton.addEventListener('click', showModal);
     cancelModal();
 }
 
-function showModal() {
+function showModal(BoardId) {
     let modal = document.getElementById('modal_container');
         modal.style.display = 'block';
+    document.getElementById('board_id').value = BoardId;
 }
 
 function cancelModal() {
@@ -29,3 +31,29 @@ function cancelModal() {
         modal.style.display = 'none';
     })
 }
+
+function addNewCard() {
+    let newCardButton = document.getElementsByClassName('addNewCardBtn');
+    let runModal = function(){
+        let BoardId = this.getAttribute("data-board-id");
+        showModal(BoardId);
+    };
+    for (let i = 0; i < newCardButton.length; i++){
+        newCardButton[i].addEventListener('click', runModal, false);
+    }
+    cancelModal();
+}
+
+/*
+*
+var classname = document.getElementsByClassName("classname");
+
+var myFunction = function() {
+    var attribute = this.getAttribute("data-myattribute");
+    alert(attribute);
+};
+
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', myFunction, false);
+}
+* */
